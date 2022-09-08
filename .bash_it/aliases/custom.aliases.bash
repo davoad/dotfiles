a993dca334a724707ff5fc1fc96d67d2b8517a7f
Adam Davidson <adam.davo@gmail.com>
2022-09-08 14:19:55 +1000

    Clean dotfiles

--- .bash_it/aliases/custom.aliases.bash
+++
@@ -1,5 +0,0 @@
-# tmux
-alias tls="tmux ls"
-alias td="tmux detach"
-alias tnew="new-tmux-from-dir-name"
-alias tatt="tmux attach-session -t "
--- .bash_it/lib/custom.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-function new-tmux-from-dir-name {
-  tmux -2 new-session -As `basename $PWD`
-}
