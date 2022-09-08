d4b9552f2003cfb28f1113e2094b6bf0720e3bb1
Adam Davidson <adam.davo@gmail.com>
2022-09-08 20:20:38 +1000

    Change stow install to cater for existing config files

--- install.sh
+++ install.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
 
-stow -v -R zsh
-stow -v -R tmux
+PACKAGES=(
+    zsh
+    tmux
+)
+
+for PKG in ${PACKAGES[@]}; do
+    CONFLICTS=$(stow --no --verbose $PKG 2>&1 | awk '/\* existing target is/ {print $NF}')
+    for filename in ${CONFLICTS[@]}; do
+        TARGETFILE=$HOME/$filename 
+        if [[ -f $TARGETFILE || -L $TARGETFILE ]]; then
+            TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+            BACKUPFILE="$TARGETFILE"_backup_"$TIMESTAMP"
+            echo "Moving $TARGETFILE to $BACKUPFILE"
+            mv $TARGETFILE $BACKUPFILE
+        fi
+    done
+
+    stow --verbose -R $PKG
+done
